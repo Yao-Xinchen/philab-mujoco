cc1989dec67dcb737f21bfdbbe23441ace6c1951
Yao-Xinchen <yao29@illinois.edu>
2025-07-28 04:07:26 -0500

    manually add encoder to export.ipynb

--- src/philab_mujoco/train_params.py
+++ src/philab_mujoco/train_params.py
@@ -39,7 +39,7 @@ def brax_ppo_config(env_name: str) -> config_dict.ConfigDict:
         rl_config.network_factory = config_dict.create(
             policy_hidden_layer_sizes=(512, 256, 128),
             value_hidden_layer_sizes=(512, 256, 128),
-            encoder_hidden_layer_sizes=(128, 64, 16),
+            encoder_hidden_layer_sizes=(128, 32, 8),
             policy_obs_key="state",
             value_obs_key="privileged_state",
             encoder_obs_key="state_history",
